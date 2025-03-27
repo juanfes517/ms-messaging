46515dfc2d83f8938ffb776c36e238c8140e85e0
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-27 18:37:17 -0500

    feat: add external service for Twilio messaging

---
+++ src/main/java/com/pragma/messaging/infrastructure/helper/constants/TwilioConstants.java
@@ -0,0 +1,8 @@
+package com.pragma.messaging.infrastructure.helper.constants;
+
+public class TwilioConstants {
+
+    private TwilioConstants() {}
+
+    public static final String TWILIO_SUCCESS_MESSAGE = "Message sent successfully";
+}
