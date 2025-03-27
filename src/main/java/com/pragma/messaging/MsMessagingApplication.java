282474192a28fafb4c2b286b460194afe4660093
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-27 14:52:15 -0500

    first commit

---
+++ src/main/java/com/pragma/messaging/MsMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/messaging/MsMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
