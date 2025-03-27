522361de928f821c048ea0992968c919e1cd6fca
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-27 15:42:18 -0500

    feat: add security settings

---
+++ src/main/java/com/pragma/messaging/infrastructure/configuration/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.pragma.messaging.infrastructure.configuration;
+
+import com.pragma.messaging.infrastructure.configuration.securityfilter.JwtTokenValidator;
+import com.pragma.messaging.infrastructure.helper.JwtUtils;
+import com.pragma.messaging.infrastructure.helper.SecurityConstants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtUtils jwtUtils;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    http.requestMatchers(SecurityConstants.getPublicEndpoints()).permitAll();
+                    http.anyRequest().authenticated();
+                })
+                .addFilterBefore(new JwtTokenValidator(jwtUtils), BasicAuthenticationFilter.class)
+                .build();
+    }
+}
