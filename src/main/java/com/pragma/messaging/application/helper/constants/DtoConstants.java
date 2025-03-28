0c184f81261379fcc50ee7ed2bb2a44504ec54bd
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-27 21:46:28 -0500

    feat: add request DTO for the message

---
+++ src/main/java/com/pragma/messaging/application/helper/constants/DtoConstants.java
@@ -0,0 +1,10 @@
+package com.pragma.messaging.application.helper.constants;
+
+public class DtoConstants {
+
+    private DtoConstants() {}
+
+    public static final String CUSTOMER_CELL_PHONE_NUMBER_CANNOT_BE_BLANK = "The customer cell phone number cannot be blank";
+    public static final String RESTAURANT_NAME_CANNOT_BE_BLANK = "The restaurant name cannot be blank";
+    public static final String SECURITY_PIN_CANNOT_BE_BLANK = "The security PIN cannot be blank";
+}
