a6ea5fdc3f1478294e5112638681908ebeae00c6
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-27 22:19:58 -0500

    feat: add endpoint that sends an order ready notification

---
+++ src/main/java/com/pragma/messaging/infrastructure/helper/constants/ApiConstants.java
@@ -0,0 +1,10 @@
+package com.pragma.messaging.infrastructure.helper.constants;
+
+public class ApiConstants {
+
+    private ApiConstants() {}
+
+    public static final String OK_DESCRIPTION = "Request successful";
+    public static final String INTERNAL_SERVER_ERROR_DESCRIPTION = "Internal server error";
+    public static final String SEND_ORDER_READY_DESCRIPTION = "Notify via Twilio that an order is ready";
+}
